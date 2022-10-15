97cb5a3494a48822a9f598cd0940ea89de2696ad
Daniel Schnetler <daniel.schnetler@gmail.com>
2022-10-15 14:59:32 +0200

    using core types

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + n1 + n2);
+    return n1 + n2;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is:";
+
+add(number1, number2, printResult, resultPhrase);
