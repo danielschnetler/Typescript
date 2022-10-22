1bb52f12612b48d9ff8324e592516a0728f13153
Daniel Schnetler <daniel.schnetler@outlook.com>
2022-10-22 10:35:44 +0200

    function overloads

--- src/app.ts
+++ src/app.ts
@@ -21,12 +21,19 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = add("Dan", "the Man");
+result.split(" ");
 
 type UnkownEmployee = Employee | Admin;
 
