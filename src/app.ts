861242a5d0a3c89dd5b6b04a869f290272d23095
Daniel Schnetler <daniel.schnetler@outlook.com>
2022-11-06 11:27:34 +0100

    fetching user input

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  // target:any, methodname: string
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
@@ -36,7 +37,7 @@ class ProjectInput {
       "#title"
     ) as HTMLInputElement;
     this.descriptionInputElement = this.element.querySelector(
-      "#descriptor"
+      "#description"
     ) as HTMLInputElement;
     this.peopleInputElement = this.element.querySelector(
       "#people"
@@ -46,10 +47,39 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
