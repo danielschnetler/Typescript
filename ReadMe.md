633f3878db4209cd45ef64d64d05d12110d4333c
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-09 13:24:03 +0100

    The rest of the CRUD operations

--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,6 @@ Run:
 `tsc --init` (Updated some configs after)  
 `npm install --save express body-parser`  
 `npm install --save-dev nodemon`  
-`npm install --save-dev @types/node  @types/express`
+`npm install --save-dev @types/node  @types/express`  
+Start the app by running:  
+`npm run start` and `tsc -w` in another terminal to watch and compile any changes.
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 import { Todo } from "../models/todos";
 
-const TODOS: Todo[] = [];
+let TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
@@ -10,4 +10,36 @@ export const createTodo: RequestHandler = (req, res, next) => {
   TODOS.push(newTodo);
 
   res.status(201).json({ message: "Created the todo", createdTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedtext = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find Todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedtext);
+
+  res.json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find Todo!");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Successfully removed todo!" });
 };
